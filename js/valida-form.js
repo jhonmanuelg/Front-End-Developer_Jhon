5ec501222b4159246de936b2b84721854d9da928
jhonmanuelg <jhonmanuelg6@outlook.com>
2021-09-13 04:12:22 -0300

    actualisando aula modulo de javascript

--- js/valida-form.js
+++ js/valida-form.js
@@ -1,23 +1,53 @@
 
 
- var form = document.getElementById("my-form");
-    
- async function handleSubmit(event) {
-   event.preventDefault();
-   var status = document.getElementById("my-form-status");
-   var data = new FormData(event.target);
-   fetch(event.target.action, {
-     method: form.method,
-     body: data,
-     headers: {
-         'Accept': 'application/json'
-     }
-   }).then(response => {
-     status.innerHTML == ("¡Gracias por tu envío!");
-     form.reset()
-   }).catch(error => {
-     status.innerHTML = "Oops! Hubo un problema al enviar su formulario ";
-   });
- }
- form.addEventListener("submit", handleSubmit)
+  const nome=document.getElementById('nome')
+  const email=document.getElementById('email')
+  const telefone=document.getElementById('assonto')
+  const form=document.getElementById('form')
+  const alertNome=document.getElementById('alertNome')
+ 
+  document.forms["datos"];
+ 
+ nome.addEventListener('click',()=>{alertNome.style.display="none";});
+ email.addEventListener('click',alertaNome);
+ 
+  form.addEventListener("submit", e=>{
+    e.preventDefault()
+   
+    let regexEmail= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let regexTelefone= /^\d{2}-\d{5}-\d{4}$/;
+    let entrar= false
+ 
+ 
+    if (nome.value.length <6){
+      alert("Insira Nome é Sobrenome") 
+    }
+    if (!regexEmail.test(email.value)){
+     alert("O correio é inválido")
+     entrar= true
+    }
+    if(!regexTelefone.test(assonto.value)){
+     alert("O número é inválido")
+     entrar= true
+    }else{
+   
+     alert("Mensagem enviado")
+     form.reset();
+         return false;
+     
+   }
+  })
+ 
+ function alertaNome(){
+   console.log('click')
+   if (nome.value === ""){
+     alertNome.innerText="Insira Nome é Sobrenome";
+     alertNome.style.display=""; 
+   }
+   if (nome.value.length>0 && nome.value.length<6){
+     alertNome.innerText="O nome e muito corto,Insira Nome é Sobrenome";
+     alertNome.style.display=""; 
+   }
+ };
+ 
 
