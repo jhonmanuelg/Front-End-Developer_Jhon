695d30849e343c0d8fecbb36533de8ab556abcb6
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-04-18 02:29:30 -0300

    Actualição do Css e Htlm

--- js/valida-form.js
+++ js/valida-form.js
@@ -1,68 +1,51 @@
+const email = document.getElementById("email")
+const assonto = document.getElementById("assonto")
+const fonm = document.getElementById("from")
+const nome = document.getElementById("nome")
+const alertNome = document.getElementById('alertNome')
 
 
-  const nome=document.getElementById('nome')
-  const email=document.getElementById('email')
-  const assonto=document.getElementById('assonto')
-  const mensagem=document.getElementById('mensagem')
-  const form=document.getElementById('form')
-  const alertNome=document.getElementById('alertNome')
- 
-  document.forms["datos"];
- 
- nome.addEventListener('click',()=>{alertNome.style.display="none";});
- email.addEventListener('click',alertaNome);
+nome.addEventListener("click", () => {
+  alertNome.style.display = "none";
+});
+email.addEventListener("click", alertaNome);
 
- 
- 
-  form.addEventListener("submit", e=>{
-    e.preventDefault()
-   
-    let regexEmail= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    let regexTelefone= /^\d{2}-\d{5}-\d{4}$/;
-    let entrar= false
- 
- 
-    if (nome.value.length <6){
-      alert("Insira Nome é Sobrenome") 
-    }
-    if (!regexEmail.test(email.value)){
-     alert("O correio é inválido")
-     entrar= true
-    }
-    if(!regexassonto.test(assonto.value)){
-     alert("O assonto é obligatorio")
-     entrar= true
-    }
-    if(!regexmensagem.test(mensagem.value)){
-      alert("O mensagemé obligatorio")
-      entrar= true
-     }else{
-   
-     alert("Mensagem enviado")
-     form.reset();
-         return false;
-         
-     
-   }
-
-   
-  })
- 
- function alertaNome(){
-   console.log('click')
-   if (nome.value === ""){
-     alertNome.innerText="Insira Nome é Sobrenome";
-     alertNome.style.display=""; 
-   }
-   if (nome.value.length>0 && nome.value.length<6){
-     alertNome.innerText="O nome e muito corto,Insira Nome é Sobrenome";
-     alertNome.style.display=""; 
-   }
-      
-   
- };
+function alertaNome() {
+  console.log("click");
+  if (nome.value === "") {
+    alertNome.innerText = "Insira Nome é Sobrenome";
+    alertNome.style.display = "";
+  }
+  if (nome.value.length > 0 && nome.value.length < 6) {
+    alertNome.innerText = "O nome e muito corto,Insira Nome é Sobrenome";
+    alertNome.style.display = "";
+  }
+}
 
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
- 
+  let regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  let entrar = false;
 
+  if (nome.value.length < 6) {
+    alert("Insira Nome é Sobrenome");
+  }
+  if (!regexEmail.test(email.value)) {
+    alert("O correio é inválido");
+    entrar = true;
+  }
+  if (!regexassonto.test(assonto.value)) {
+    alert("O assonto é obligatorio");
+    entrar = true;
+  }
+  if (!regexmensagem.test(mensagem.value)) {
+    alert("O mensagemé obligatorio");
+    entrar = true;
+  } else {
+    alert("Mensagem enviado");
+    form.reset();
+    return false;
+  }
+});
