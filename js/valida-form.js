cda1e88a8119f26960207889d4068c537a254fca
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-04-24 21:33:31 -0300

    Modicação do titulo

--- js/valida-form.js
+++ js/valida-form.js
@@ -1,51 +1,52 @@
-const email = document.getElementById("email")
-const assonto = document.getElementById("assonto")
-const fonm = document.getElementById("from")
-const nome = document.getElementById("nome")
-const alertNome = document.getElementById('alertNome')
+// const email = document.getElementById("email")
+// const assonto = document.getElementById("assonto")
+// const nome = document.getElementById("nome")
+// const alertNome = document.getElementById('alertNome')
 
 
-nome.addEventListener("click", () => {
-  alertNome.style.display = "none";
-});
-email.addEventListener("click", alertaNome);
+// nome.addEventListener("click", () => {
+//   alertNome.style.display = "none";
+// });
+// email.addEventListener("click", alertaNome);
 
-function alertaNome() {
-  console.log("click");
-  if (nome.value === "") {
-    alertNome.innerText = "Insira Nome é Sobrenome";
-    alertNome.style.display = "";
-  }
-  if (nome.value.length > 0 && nome.value.length < 6) {
-    alertNome.innerText = "O nome e muito corto,Insira Nome é Sobrenome";
-    alertNome.style.display = "";
-  }
-}
+// function alertaNome() {
+//   console.log("click");
+//   if (nome.value === "") {
+//     alertNome.innerText = "Insira Nome é Sobrenome";
+//     alertNome.style.display = "";
+//   }
+//   if (nome.value.length > 0 && nome.value.length < 6) {
+//     alertNome.innerText = "O nome e muito corto,Insira Nome é Sobrenome";
+//     alertNome.style.display = "";
+//   }
+// }
 
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  let regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-  let entrar = false;
+//   let regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+//   let entrar = false;
 
-  if (nome.value.length < 6) {
-    alert("Insira Nome é Sobrenome");
-  }
-  if (!regexEmail.test(email.value)) {
-    alert("O correio é inválido");
-    entrar = true;
-  }
-  if (!regexassonto.test(assonto.value)) {
-    alert("O assonto é obligatorio");
-    entrar = true;
-  }
-  if (!regexmensagem.test(mensagem.value)) {
-    alert("O mensagemé obligatorio");
-    entrar = true;
-  } else {
-    alert("Mensagem enviado");
-    form.reset();
-    return false;
-  }
-});
+//   if (nome.value.length < 6) {
+//     alert("Insira Nome é Sobrenome");
+//   }
+//   if (!regexEmail.test(email.value)) {
+//     alert("O correio é inválido");
+//     entrar = true;
+//   }
+//   if (!regexassonto.test(assonto.value)) {
+//     alert("O assonto é obligatorio");
+//     entrar = true;
+//   }
+//   if (!regexmensagem.test(mensagem.value)) {
+//     alert("O mensagemé obligatorio");
+//     entrar = true;
+//   } else {
+//     alert("Mensagem enviado");
+//     form.reset();
+//     return false;
+//   }
+// });
+
+
