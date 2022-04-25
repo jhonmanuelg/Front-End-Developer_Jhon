aea18baf9d06c6794b126438807ecf25a803e566
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-04-24 21:37:08 -0300

    Modicação do titulo

--- js/mensagem.js
+++ js/mensagem.js
@@ -1,23 +1,23 @@
-const $form = document.querySelector('#form')
-$form.addEventListener('submit', envial)
+// const $form = document.querySelector('#form')
+// $form.addEventListener('submit', envial)
 
-async function envial(event) {
+// async function envial(event) {
 
-    event.preventDefault()
+//     event.preventDefault()
 
-    const form = new FormData(this)
+//     const form = new FormData(this)
 
-    const response = await fetch(this.actions, {        
-        method: this.method,
-        body: form,
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    if(response.ok){
-        this.reset()
-        alert('Obrigado por Teu Mensagem 🥰')
-    }
+//     const response = await fetch(this.actions, {        
+//         method: this.method,
+//         body: form,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     })
+//     if(response.ok){
+//         this.reset()
+//         alert('Obrigado por Teu Mensagem 🥰')
+//     }
    
-}
+// }
 
