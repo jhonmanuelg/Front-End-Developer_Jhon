65855bfbfcd35ed7236f1adc7101ad2871615edd
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-13 06:09:07 -0300

    Subendo: Atualição do CSS de Projeto do portafolho

---
+++ php/enviar.php
@@ -0,0 +1,27 @@
+<!-- <?php
+// llamar a los campo
+
+//$nome = $_POST['nome'];
+//$assunto = $_POST['assunto'];
+//$phone = $_POST['phone'];
+//$email = $_POST['email'];
+//$mensaje = $_POST['mensaje'];
+
+// datos para el E-mail
+//$destinatario = "jhonmanuelg6@outlook.com";
+//$asunto = "Contacto desde mi Portafolho";
+//
+//$carta = "DE: $nome \n";
+//$carta .= "assunto: $assunto \n";
+//$carta .= "phone: $phone \n";
+//$carta .= "email: $email \n";
+//$carta .= "mensaje: $mensaje";
+
+// enviando mensaje
+
+//mail($destinatario, $asunto, $carta);
+
+//header('location:mensage-envio.html')
+
+
+?> -->
