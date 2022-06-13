71d514d58e3a9596d2a490fd0c1b88127390d430
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-13 00:51:43 -0300

    Subendo: Validação do formulario js jquery

--- js/valida-form.js
+++ js/valida-form.js
@@ -1,50 +1,109 @@
-function validation(){
-    
-    var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error_message");
-    var text;
+$(document).ready(function () {
+  $("#btnSend").click(function () {
+    var errores = "";
 
-    error_message.style.padding = "10px";
-   
-
-    if(nome.length < 5){
-        text = "O nome é inválido";
-        error_message.innerHTML = text;
-        return false;
-    }
-   
-    if(subject.length < 10){
-        text = "O Assunto é inválido";
-        error_message.innerHTML = text;
-        return false;
-    }
-    if(isNaN(phone) || phone.length != 9){
-        text = "O número de celular é inválido só (DDD)9xx.xx.xx";
-        error_message.innerHTML = text;
-        return false;
-    }
-    if(email.indexOf("^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$") == - 1 || email.length < 20){
-        text = "O número de E-mail é inválido";
-        error_message.innerHTML = text;
-        return false;
-    }
-    if(message.length <= 300){
-        text = "A Mensagem leva apenas 300 corater";
-        error_message.innerHTML = text;
-        return false;
+    // Validado Nombre ==============================
+    if ($("#names").val() == "") {
+      errores += "<p>Escreva seu nome</p>";
+      $("#names").css("border-bottom-color", "#F14B4B");
+    } else {
+      $("#names").css("border-bottom-color", "#d1d1d1");
     }
 
-    alert("Formulário enviado com sucesso")
-    validation();
+    // validar assunto =============================
+    if ($("#assunto").val() == "") {
+      errores += "<p>Escreva seu assunto</p>";
+      $("#assunto").css("border-bottom-color", "#F14B4B");
+    } else {
+      $("#assunto").css("border-bottom-color", "#d1d1d1");
+    }
 
- return true;
- 
+    // validar assunto =============================
+    if ($("#phone").val() == "") {
+        errores += "<p>Escreva sue Celular</p>";
+        $("#phone").css("border-bottom-color", "#F14B4B");
+      } else {
+        $("#phone").css("border-bottom-color", "#d1d1d1");
+      }
 
-}
+    // Validado Correo ==============================
+    if ($("#email").val() == "") {
+      errores += "<p>Digite sue E-mail</p>";
+      $("#email").css("border-bottom-color", "#F14B4B");
+    } else {
+      $("#email").css("border-bottom-color", "#d1d1d1");
+    }
 
+    // Validado Mensaje ==============================
+    if ($("#mensaje").val() == "") {
+      errores += "<p>Escreve sue mensagem</p>";
+      $("#mensaje").css("border-bottom-color", "#F14B4B");
+    } else {
+      $("#mensaje").css("border-bottom-color", "#d1d1d1");
+    }
 
+    // ENVIANDO MENSAJE ============================
+    if ((errores == "") == false) {
+      var mensajeModal =
+        '<div class="modal_wrap">' +
+        '<div class="mensaje_modal">' +
+        "<h3>Erros encontrados</h3>" +
+        errores +
+        '<span id="btnClose">Cerrar</span>' +
+        "</div>" +
+        "</div>";
 
+      $("body").append(mensajeModal);
+    }
+
+    // CERRANDO MODAL ==============================
+    $("#btnClose").click(function () {
+      $(".modal_wrap").remove();
+    });
+  });
+});
+
+// function validation(){
+
+//     var nome = document.getElementById("name").value;
+//     var assunto = document.getElementById("assunto").value;
+//     var phone = document.getElementById("phone").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
+//     var error_message = document.getElementById("error_message");
+//     var text;
+
+//     error_message.style.padding = "10px";
+
+//     if(nome.length < 5){
+//         text = "O nome é inválido";
+//         error_message.innerHTML = text;
+//         return false;
+//     }
+
+//     if(assunto.length < 10){
+//         text = "O Assunto é inválido";
+//         error_message.innerHTML = text;
+//         return false;
+//     }
+//     if(isNaN(phone) || phone.length != 9){
+//         text = "O número de celular é inválido só (DDD)9xx.xx.xx";
+//         error_message.innerHTML = text;
+//         return false;
+//     }
+//     if(email.indexOf("^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$") == - 1 || email.length < 20){
+//         text = "O número de E-mail é inválido";
+//         error_message.innerHTML = text;
+//         return false;
+//     }
+//     if(message.length <= 300){
+//         text = "A Mensagem leva apenas 300 corater";
+//         error_message.innerHTML = text;
+//         return false;
+//     }
+
+//     validation();
+
+//  return true;
+
+// }
