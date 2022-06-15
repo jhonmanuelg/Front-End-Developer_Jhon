285c9d9f6c526415b15faea3630fd8aa93d9c274
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-15 15:59:46 -0300

    Subindo: Base de datos php

--- php/enviar.php
+++ php/enviar.php
@@ -1,27 +1,35 @@
-<!-- <?php
+<?php
+include_once ("conection.php"){
+    conectionPROTAFOLHO::conectionPROTAFOLHO():
+}
+
 // llamar a los campo
 
-//$nome = $_POST['nome'];
-//$assunto = $_POST['assunto'];
-//$phone = $_POST['phone'];
-//$email = $_POST['email'];
-//$mensaje = $_POST['mensaje'];
+$nomes = $_POST['nomes'];
+$assunto = $_POST['assunto'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
 // datos para el E-mail
-//$destinatario = "jhonmanuelg6@outlook.com";
-//$asunto = "Contacto desde mi Portafolho";
-//
-//$carta = "DE: $nome \n";
-//$carta .= "assunto: $assunto \n";
-//$carta .= "phone: $phone \n";
-//$carta .= "email: $email \n";
-//$carta .= "mensaje: $mensaje";
+
+$destinatario = "jhonmanuelg6@outlook.com";
+
+$asunto = "Contacto desde mi Portafolho"
+
+$carta = "DE: $nomes \n";
+$carta .= "assunto: $assunto \n";
+$carta .= "phone: $phone \n";
+$carta .= "email: $email \n";
+$carta .= "mensaje: $mensaje";
 
 // enviando mensaje
 
-//mail($destinatario, $asunto, $carta);
 
-//header('location:mensage-envio.html')
+@mail($destinatario, $asunto, $carta);
 
 
-?> -->
+header('location:mensage-envio.html')
+
+
+?>
