f699498e7ab86be532002941a5209dcc39ae69e0
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-17 05:36:12 -0300

    Subindo: modificada a Base de dato

---
+++ js/formspree.js
@@ -0,0 +1,32 @@
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handlesubmit);
+
+async function handlesubmit(event) {
+  event.preventDefault();
+
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body : form,
+    header: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    var mensajeModal =
+    '<div class="modal_wrap">' +
+    '<div class="mensaje_modal">' +
+    "<h3>E-mail enviado com sucesso</h3>" +
+    '<span id="btnClose">Cerrar</span>' +
+    "</div>" +
+    "</div>";
+
+  $("body").append(mensajeModal);
+  }
+  $("#btnClose").click(function () {
+    $(".modal_wrap").remove();
+  });
+
+}
--- js/valida-form.js
+++ js/valida-form.js
@@ -1,46 +1,49 @@
 $(document).ready(function () {
-  $("#btnSend").click(function () {
-    var errores = "";
+
+  $("#btnSend").click(function () {   
+    
+    var errores = "";  
+    
 
     // Validado Nombre ==============================
     if ($("#names").val() == "") {
       errores += "<p>Escreva seu nome</p>";
-      $("#names").css("border-bottom-color", "#f9ca24");
+      $("#names").css("border-left-color", "#f9ca24");
     } else {
-      $("#names").css("border-bottom-color", "#222");
-    }
+      $("#names").css("border-left-color", "#222");
+    };
 
     // validar assunto =============================
     if ($("#assunto").val() == "") {
       errores += "<p>Escreva seu assunto</p>";
-      $("#assunto").css("border-bottom-color", "#f9ca24");
+      $("#assunto").css("border-left-color", "#f9ca24");
     } else {
-      $("#assunto").css("border-bottom-color", "#222");
-    }
+      $("#assunto").css("border-left-color", "#222");
+    };
 
     // validar assunto =============================
     if ($("#phone").val() == "") {
         errores += "<p>Escreva sue Celular</p>";
-        $("#phone").css("border-bottom-color", "#f9ca24");
+        $("#phone").css("border-left-color", "#f9ca24");
       } else {
-        $("#phone").css("border-bottom-color", "#222");
-      }
+        $("#phone").css("border-left-color", "#222");
+      };
 
     // Validado Correo ==============================
     if ($("#email").val() == "") {
       errores += "<p>Digite sue E-mail</p>";
-      $("#email").css("border-bottom-color", "#f9ca24");
+      $("#email").css("border-left-color", "#f9ca24");
     } else {
-      $("#email").css("border-bottom-color", "#222");
-    }
+      $("#email").css("border-left-color", "#222");
+    };
 
     // Validado Mensaje ==============================
     if ($("#mensaje").val() == "") {
       errores += "<p>Escreve sue mensagem</p>";
-      $("#mensaje").css("border-bottom-color", "#f9ca24");
+      $("#mensaje").css("border-left-color", "#f9ca24");
     } else {
-      $("#mensaje").css("border-bottom-color", "#222");
-    }
+      $("#mensaje").css("border-left-color", "#222");
+    };
 
     // ENVIANDO MENSAJE ============================
     if ((errores == "") == false) {
@@ -54,13 +57,22 @@ $(document).ready(function () {
         "</div>";
 
       $("body").append(mensajeModal);
-    }
+    };
+   
 
     // CERRANDO MODAL ==============================
     $("#btnClose").click(function () {
       $(".modal_wrap").remove();
     });
+
+    
+    
   });
+  
+  // metodo para mandar el email
+  
+
+
 });
 
 
