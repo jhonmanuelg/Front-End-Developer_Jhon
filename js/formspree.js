d355c24bbd41e5fbde3fa414798151c858c5189e
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-10-14 20:43:47 -0300

    Subendo: modicicação no FormData

--- js/formspree.js
+++ js/formspree.js
@@ -1,34 +1,31 @@
 const $form = document.querySelector("#form");
 
-$form.addEventListener("submit", handlesubmit);
+$form.addEventListener("submit", btnSend);
 
-async function handlesubmit(event) {
+async function btnSend(event) {
   event.preventDefault();
 
   const form = new FormData(this);
   const response = await fetch(this.action, {
     method: this.method,
-    body : form,
+    body: form,
     header: {
-      Accept: "application/json",
+      Accept: "application/Json",
     },
   });
   if (response.ok) {
     this.reset();
     var mensajeModal =
-    '<div class="modal_wrap">' +
-    '<div class="mensaje_modal">' +
-    "<h1>✔</h1> <h3>E-mail Enviado</h3>" +
-    '<span id="btnClose">Cerrar</span>' +
-    "</div>" +
-    "</div>";
+      '<div class="modal_wrap">' +
+      '<div class="mensaje_modal">' +
+      "<h1>✔</h1> <h3>E-mail Enviado</h3>" +
+      '<span id="btnClose">Cerrar</span>' +
+      "</div>" +
+      "</div>";
 
-  $("body").append(mensajeModal);
+    $("body").append(mensajeModal);
   }
   $("#btnClose").click(function () {
     $(".modal_wrap").remove();
   });
-
 }
-
-
