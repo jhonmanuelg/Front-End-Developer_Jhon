a505291ad53f2313682b3a6b829e7398c84e673d
Jhon Manuel <jhonmanuelg6@outlook.com>
2023-05-01 14:56:54 -0300

    Uploading: Changing the image format for .web peso V_10

--- js/script.js
+++ js/script.js
@@ -37,3 +37,38 @@ $(document).ready(function() {
 
 });
 
+
+
+// para ocultar texto 1 presentacion
+
+let btne_ver_mas = document.getElementById("btne_ver_mas");
+let btne_ver_menos = document.getElementById("btne_ver_menos");
+let span_text = document.getElementById("span_text");
+
+btne_ver_mas.addEventListener("click", act_style);
+
+function act_style(){
+    span_text.classList.add("mostrar");
+    btne_ver_mas.classList.add("ocultar");
+}
+
+btne_ver_menos.addEventListener("click", ocultarTexto);
+
+function ocultarTexto(){
+    span_text.classList.remove("mostrar")
+
+  if(span_text.classList.contains("mostrar")){
+    btne_ver_mas.classList.add("mostrar")
+  }else{
+    btne_ver_mas.classList.remove("ocultar")
+  }
+}
+
+// función de WhatsApp
+const botaoWhatsapp = document.querySelector('#WhatsApp-link');
+
+botaoWhatsapp.addEventListener('click', function() {
+  window.open('https://wa.me/11958838782', '_blank');
+});
+
+
